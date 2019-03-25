6f02b627f459a0399950a3be40a0d90ce640017e
84181876@qq.com <84181876@qq.com>
2019-03-25 18:17:27 +0800

    init

---
+++ jframework-basic/src/main/java/com/roc/jframework/basic/utils/SetUtils.java
@@ -0,0 +1,13 @@
+package com.roc.jframework.basic.utils;
+
+import java.util.Set;
+
+public class SetUtils {
+
+    public static boolean isNullOrEmpty(Set set){
+        if(set == null){
+            return true;
+        }
+        return set.isEmpty();
+    }
+}

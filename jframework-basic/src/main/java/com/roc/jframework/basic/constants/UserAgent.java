f4b7376d87a2df446292c3b6c4153a4473c5a7d9
84181876@qq.com <84181876@qq.com>
2019-03-28 15:06:36 +0800

    init

---
+++ jframework-basic/src/main/java/com/roc/jframework/basic/constants/UserAgent.java
@@ -0,0 +1,7 @@
+package com.roc.jframework.basic.constants;
+
+public class UserAgent {
+
+    public static final String CHROME = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36";
+
+}

4df96adf2e149bcc4f0b899b73da796d6566ac3d
84181876@qq.com <84181876@qq.com>
2019-03-29 12:10:39 +0800

    init

--- jframework-core/src/test/java/com/roc/jframework/core/RequestsTest.java
+++ jframework-core/src/test/java/com/roc/jframework/core/RequestsTest.java
@@ -20,7 +20,7 @@ public class RequestsTest {
 //                .param("y", "433")
                 .userAgent(UserAgent.CHROME)
 //                .showUrl(true)
-                .url("https://www.kawebook.com/story/97/%E0%B8%99%E0%B8%B4%E0%B8%A2%E0%B8%B2%E0%B8%A2/%E0%B8%99%E0%B8%B4%E0%B8%A2%E0%B8%B2%E0%B8%A2-%E0%B8%A3%E0%B8%B1%E0%B8%81/%E0%B9%80%E0%B8%88%E0%B9%89%E0%B8%B2%E0%B8%AA%E0%B8%B2%E0%B8%A7%E0%B8%A1%E0%B8%B7%E0%B8%AD%E0%B9%83%E0%B8%AB%E0%B8%A1%E0%B9%88%E0%B9%81%E0%B8%AB%E0%B9%88%E0%B8%87%E0%B8%AA%E0%B8%81%E0%B8%A5%E0%B8%A5%E0%B8%B9%E0%B9%88")
+                .url("https://www.novelhit.com/novels/6678/")
                 .get();
         System.out.println(HttpClientUtils.getContent(r));
         HttpClientUtils.close(r);

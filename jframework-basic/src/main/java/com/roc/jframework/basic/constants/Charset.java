8d567172ab07d9c4081ee5581447ac38ac914503
84181876@qq.com <84181876@qq.com>
2019-04-01 14:42:33 +0800

    init

---
+++ jframework-basic/src/main/java/com/roc/jframework/basic/constants/Charset.java
@@ -0,0 +1,6 @@
+package com.roc.jframework.basic.constants;
+
+public class Charset {
+
+    private static final String UTF8 = "utf-8";
+}
---
+++ jframework-basic/src/main/java/com/roc/jframework/basic/constants/ContentType.java
@@ -0,0 +1,7 @@
+package com.roc.jframework.basic.constants;
+
+public class ContentType {
+
+    public static final String APPLICATION_JSON = "application/json";
+
+}

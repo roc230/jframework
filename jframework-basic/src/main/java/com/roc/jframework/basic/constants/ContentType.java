b86f2537eb31dcb1db1a06a222e475b0fa71f33d
84181876@qq.com <84181876@qq.com>
2019-04-03 10:40:50 +0800

    init

--- jframework-basic/src/main/java/com/roc/jframework/basic/constants/ContentType.java
+++ jframework-basic/src/main/java/com/roc/jframework/basic/constants/ContentType.java
@@ -6,4 +6,16 @@ public class ContentType {
 
     public static final String APPLICATION_FORM = "application/x-www-form-urlencoded";
 
+    public static final String PLAIN_TEXT = "plain/text";
+
+    public static final String PLAIN_HTML = "plain/html";
+
+    public static final String APPLICATION_XML = "application/xml";
+
+    public static final String IMAGE_JPEG = "image/jpeg";
+
+    public static final String APPLICATION_JAVASCRIPT = "application/javascript";
+
+    public static final String MULTIPART_FORM_DATA = "multipart/form-data";
+
 }

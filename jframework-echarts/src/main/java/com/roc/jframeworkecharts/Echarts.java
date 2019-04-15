6754098f4416eae48045022d7470066cf2fde2a0
84181876@qq.com <84181876@qq.com>
2019-04-15 15:50:56 +0800

    init

--- jframework-echarts/src/main/java/com/roc/jframeworkecharts/Echarts.java
+++ jframework-echarts/src/main/java/com/roc/jframeworkecharts/Echarts.java
@@ -1,6 +1,60 @@
 package com.roc.jframeworkecharts;
 
+import com.roc.jframeworkecharts.model.bar.BarOption;
+import com.roc.jframeworkecharts.model.funnel.FunnelOption;
+import com.roc.jframeworkecharts.model.line.LineOptionBuilder;
+import com.roc.jframeworkecharts.model.linebar.LineBarOption;
+import com.roc.jframeworkecharts.model.pie.PieOption;
+import com.roc.jframeworkecharts.model.radar.RadarOption;
+
 public class Echarts {
 
+    /**
+     * 拆线图
+     * @return
+     */
+    public static LineOptionBuilder lineOptionBuilder(){
+        return LineOptionBuilder.create();
+    }
+
+    /**
+     * 条形图
+     * @return
+     */
+    public static BarOption.Builder barOptionBuilder(){
+        return new BarOption.Builder();
+    }
+
+    /**
+     * 拆线条形图混合
+     * @return
+     */
+    public static LineBarOption.Builder lineBarOptionBuilder(){
+        return new LineBarOption.Builder();
+    }
+
+    /**
+     * 饼图
+     * @return
+     */
+    public static PieOption.Builder pieOptionBuilder(){
+        return new PieOption.Builder();
+    }
+
+    /**
+     * 雷达图
+     * @return
+     */
+    public static RadarOption.Builder radarOptionBuilder(){
+        return new RadarOption.Builder();
+    }
+
+    /**
+     * 漏斗图
+     * @return
+     */
+    public static FunnelOption.Builder funnelOptionBuilder(){
+        return new FunnelOption.Builder();
+    }
 
 }

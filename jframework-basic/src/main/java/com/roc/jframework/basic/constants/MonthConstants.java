d7528fae9cc67f34a46a3445e93e0be6eb4fcd06
84181876@qq.com <84181876@qq.com>
2019-04-15 12:37:45 +0800

    init

---
+++ jframework-basic/src/main/java/com/roc/jframework/basic/constants/MonthConstants.java
@@ -0,0 +1,14 @@
+package com.roc.jframework.basic.constants;
+
+/**
+ * 月份常量
+ */
+public class MonthConstants {
+    /**
+     * "1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"
+     * @return
+     */
+    public static String[] MONTH_12(){
+        return new String[]{"1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"};
+    }
+}

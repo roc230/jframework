d9979fce44ffad5d8ba4b9d3f4f47a9aba9562d2
84181876@qq.com <84181876@qq.com>
2019-07-01 18:12:51 +0800

    init

---
+++ jframework-echarts/src/main/java/com/roc/jframeworkecharts/JframeworkEchartsApplication.java
@@ -0,0 +1,13 @@
+package com.roc.jframeworkecharts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JframeworkEchartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JframeworkEchartsApplication.class, args);
+    }
+
+}

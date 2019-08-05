f0ac54b5f2c4f1fe64f926b0525a44aa9559f5fe
84181876@qq.com <84181876@qq.com>
2019-08-05 17:29:19 +0800

    init

--- jframework-core/src/main/java/com/roc/jframework/core/component/myhttpclient/HttpResponse.java
+++ jframework-core/src/main/java/com/roc/jframework/core/component/myhttpclient/HttpResponse.java
@@ -1,5 +1,8 @@
 package com.roc.jframework.core.component.myhttpclient;
 
+import com.roc.jframework.core.utils.JsoupUtils;
+import org.jsoup.nodes.Document;
+
 public class HttpResponse {
 
     private String content;
@@ -10,5 +13,10 @@ public class HttpResponse {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public Document asJsoupDocument(){
+        Document doc = JsoupUtils.parse(this.content);
+        return doc;
     }
 }

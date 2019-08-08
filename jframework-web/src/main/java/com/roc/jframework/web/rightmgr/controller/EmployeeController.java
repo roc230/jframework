926b725499edbf7703f31880e4cc1884009f433d
84181876@qq.com <84181876@qq.com>
2019-08-08 17:58:30 +0800

    init

--- jframework-web/src/main/java/com/roc/jframework/web/rightmgr/controller/EmployeeController.java
+++ jframework-web/src/main/java/com/roc/jframework/web/rightmgr/controller/EmployeeController.java
@@ -1,16 +1,96 @@
 package com.roc.jframework.web.rightmgr.controller;
 
+import com.roc.jframework.basic.constants.ServiceResult;
+import com.roc.jframework.basic.utils.StringUtils;
+import com.roc.jframework.web.entity.ExeResult;
+import com.roc.jframework.web.rightmgr.entity.SysEmployee;
+import com.roc.jframework.web.rightmgr.service.IEmployeeService;
 import com.roc.jframework.web.rightmgr.vo.EmployeeVO;
+import com.roc.jframework.web.rightmgr.vo.PageVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequestMapping("/employee")
 @Controller
 public class EmployeeController {
 
+    @Autowired
+    private IEmployeeService employeeService;
+
+    @GetMapping("/page")
+    @ResponseBody
     public List<EmployeeVO> getPage(){
         return null;
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public EmployeeVO get(@PathVariable("id") String employeeId){
+        return null;
+    }
+
+    @PostMapping("/save")
+    @ResponseBody
+    public String saveEmployee(@RequestBody EmployeeVO employeeVO){
+        SysEmployee employee = new SysEmployee();
+        employee.setEnable(true);
+        employee.setName(employeeVO.getName());
+        employee.setBirthday(employeeVO.getBirthday());
+        employee.setJoinTime(employeeVO.getJoinTime());
+        employee.setSex(employeeVO.getSex());
+
+        String[] ids = employeeVO.getOrgIds() != null ? employeeVO.getOrgIds()[0] : new String[0];
+        this.employeeService.add(employee, ids, employeeVO.getAccount());
+        return "SUCCESS";
+    }
+
+    @PostMapping("/update")
+    @ResponseBody
+    public String updateEmployee(@RequestBody EmployeeVO employeeVO){
+        return null;
+    }
+
+    @PostMapping("/delete")
+    @ResponseBody
+    public ExeResult deleteEmployee(@RequestParam("id") String employeeId){
+        ExeResult r = this.employeeService.delete(employeeId);
+        return r;
+    }
+
+    @GetMapping("/page/{pageNum}/{pageSize}")
+    @ResponseBody
+    public PageVO<EmployeeVO> getByPages(@PathVariable("pageNum") Integer pageNum, @PathVariable("pageSize") Integer pageSize){
+        ExeResult<Page<SysEmployee>> page = this.employeeService.getPage(pageNum - 1, pageSize);
+        PageVO<EmployeeVO> pageVO = new PageVO<EmployeeVO>(page.getObj()) {
+            @Override
+            public <K> EmployeeVO transform(K obj) {
+                SysEmployee emp = (SysEmployee)obj;
+                EmployeeVO vo = new EmployeeVO();
+                vo.setId(emp.getId());
+                vo.setName(emp.getName());
+                if(StringUtils.isNullOrEmpty(emp.getSex())){
+                    vo.setSex("");
+                }else if (emp.getSex().equals("male")){
+                    vo.setSex("男");
+                }else if(emp.getSex().equals("female")){
+                    vo.setSex("女");
+                }
+                String orgNames = "";
+                orgNames += emp.getCompany() != null ? emp.getCompany().getName() : "";
+                orgNames += emp.getDepartment() != null ? "/" + emp.getDepartment().getName() : "";
+                orgNames += emp.getStation() != null ? "/" + emp.getStation().getName() : "";
+                vo.setOrgNames(orgNames);
+                vo.setAccount(emp.getAccount() != null ? emp.getAccount().getLoginName() : "");
+                vo.setJoinTime(emp.getJoinTime());
+                vo.setBirthday(emp.getBirthday());
+                return vo;
+            }
+        };
+        return pageVO;
     }
 }
--- jframework-web/src/main/java/com/roc/jframework/web/rightmgr/controller/OrganizationController.java
+++ jframework-web/src/main/java/com/roc/jframework/web/rightmgr/controller/OrganizationController.java
@@ -1,8 +1,9 @@
 package com.roc.jframework.web.rightmgr.controller;
 
+import com.roc.jframework.basic.constants.ServiceResult;
 import com.roc.jframework.basic.ext.KeyValue;
+import com.roc.jframework.basic.utils.ListUtils;
 import com.roc.jframework.basic.utils.StringUtils;
-import com.roc.jframework.web.accountmgr.entity.OrgType;
 import com.roc.jframework.web.rightmgr.entity.OrganizationType;
 import com.roc.jframework.web.rightmgr.entity.SysOrganization;
 import com.roc.jframework.web.rightmgr.service.IOrganizationService;
@@ -32,6 +33,29 @@ public class OrganizationController {
         o.setType(OrganizationType.valueOf(orgVO.getType()));
         this.organizationService.add(o, orgVO.getParentId(), null);
         return "success";
+    }
+
+    @PostMapping("/update")
+    @ResponseBody
+    public String updateOrg(@RequestBody OrgVO orgVO){
+        SysOrganization o = new SysOrganization();
+        o.setId(orgVO.getId());
+        o.setDescription(orgVO.getDescription());
+        o.setName(orgVO.getName());
+        o.setCode(orgVO.getCode());
+        o.setType(OrganizationType.valueOf(orgVO.getType()));
+        this.organizationService.update(o, orgVO.getParentId(), null);
+        return "success";
+    }
+
+    @PostMapping("/delete")
+    @ResponseBody
+    public String deleteOrg(@RequestParam("orgId") String orgId){
+        if(StringUtils.isNullOrEmpty(orgId)){
+            return "FAILED";
+        }
+        ServiceResult r = this.organizationService.delete(orgId);
+        return r.getCode();
     }
 
     @GetMapping("/tree")
@@ -66,6 +90,22 @@ public class OrganizationController {
             levelvos = new ArrayList<>(tmplist);
         }
 
+        //把页节点的children设置为null
+        List<OrgVO> levelnodes = new ArrayList<>(roots);
+        roots = levelnodes;
+        while(levelnodes.size() > 0){
+            List<OrgVO> nextlevelnodes = new ArrayList<>();
+            for(OrgVO vo : levelnodes){
+                if(vo.getChildren() != null && vo.getChildren().size() > 0){
+                    nextlevelnodes.addAll(vo.getChildren());
+                }else{
+                    vo.setChildren(null);
+                }
+            }
+            levelnodes = new ArrayList<>(nextlevelnodes);
+        }
+
+
         return roots;
     }
 
@@ -75,6 +115,7 @@ public class OrganizationController {
         vo.setName(org.getName());
         vo.setCode(org.getCode());
         vo.setLabel(org.getName());
+        vo.setValue(org.getId());
         vo.setDescription(org.getDescription());
         vo.setType(org.getType().name());
         vo.setParentId(org.getParent() != null ? org.getParent().getId() : null);
@@ -107,5 +148,16 @@ public class OrganizationController {
             }
         }
         return kvs;
+    }
+
+    @GetMapping("/children/{id}")
+    @ResponseBody
+    public List<OrgVO> getChildren(@PathVariable("id") String orgId){
+        List<SysOrganization> children = this.organizationService.getChildren(orgId);
+        if(ListUtils.isNullOrEmpty(children)){
+            return ListUtils.newArrayList();
+        }
+
+        return toVO(children);
     }
 }

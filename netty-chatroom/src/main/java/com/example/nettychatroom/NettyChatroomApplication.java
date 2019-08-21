186ef83fc01d066efc96a4a1d2237f880093414d
84181876@qq.com <84181876@qq.com>
2019-08-21 16:50:46 +0800

    init

---
+++ netty-chatroom/src/main/java/com/example/nettychatroom/NettyChatroomApplication.java
@@ -0,0 +1,13 @@
+package com.example.nettychatroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyChatroomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyChatroomApplication.class, args);
+    }
+
+}
